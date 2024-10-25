088fdf92e1b2f106cb0c4466459203a9259551bd
LPT-27 <LPT-27@10.0.0.105>
2024-10-25 15:43:41 +0530

    third commkit

---
+++ svkp_files/src/basicsofjava/Array.java
@@ -0,0 +1,13 @@
+package basicsofjava;
+
+public class Array 
+{
+	public static void main(String[] args)
+	{
+		int number[]= {5,6,7,8,9,10};
+		System.out.println(number[2]);
+		System.out.println(number[5]);
+	}
+}
+	
+
